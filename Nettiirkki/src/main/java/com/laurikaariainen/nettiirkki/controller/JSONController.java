6b16f7218672cdfb79fb4cfcac4c2927cd71a7cb
lauri kääriäinen <a1102157@myy.haaga-helia.fi>
2014-01-14 03:46:22 +0200

    Atmosphere instead of Meteor implemented, had to hack SecurityContextHolder a bit because AJAX-call made backend to forget SecurityContext while session was still remembered.

--- Nettiirkki/src/main/java/com/laurikaariainen/nettiirkki/controller/JSONController.java
+++ Nettiirkki/src/main/java/com/laurikaariainen/nettiirkki/controller/JSONController.java
@@ -2,6 +2,7 @@ package com.laurikaariainen.nettiirkki.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.security.Principal;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.util.Date;
@@ -11,11 +12,17 @@ import java.util.Locale;
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.stream.JsonGenerator;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,15 +32,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.laurikaariainen.nettiirkki.bean.Channel;
 import com.laurikaariainen.nettiirkki.service.ChannelService;
 
-
 import org.atmosphere.cpr.ApplicationConfig;
 import org.atmosphere.cpr.AtmosphereResource;
 import org.atmosphere.cpr.Broadcaster;
 import org.atmosphere.cpr.BroadcasterFactory;
 import org.atmosphere.cpr.FrameworkConfig;
 import org.atmosphere.cpr.HeaderConfig;
-import org.atmosphere.cpr.Meteor;
+
 import org.atmosphere.websocket.WebSocketEventListenerAdapter;
+
 
 
 
@@ -60,10 +67,15 @@ public class JSONController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, Model model, HttpServletRequest request, Authentication authentication) {
 		logger.info("Welcome home! The client locale is {}.", locale);
+		for(Cookie cookie : request.getCookies())
+			System.out.println(cookie.getName() +":"+ cookie.getValue());
+		System.out.println(authentication);
 		
-	
+		//System.out.println("JSESSIONID:"+request.getHeader("jsessionid"));
+		//System.out.println("ID:"+request.getHeader("id"));
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -159,9 +171,10 @@ public class JSONController {
 	 */
 	
 	@RequestMapping(value ="websocket/{name}", method = RequestMethod.GET)
-	public void getChannel(@PathVariable String name, HttpServletResponse response, HttpServletRequest request) throws IOException{
-		response.setContentType("application/json");
+	public void getChannel(@PathVariable String name,Model model, HttpServletResponse response, HttpServletRequest request, Principal principal, Authentication authentication) throws IOException{
+		//response.setContentType("application/json");
 		
+		System.out.println("is this reached ever?");
 		
 		boolean wasFound = false;
 		for (String channel : CHANNELS){
@@ -171,13 +184,47 @@ public class JSONController {
 				break;
 			}
 		}
-		if(wasFound == false)
+		if(wasFound == false){
+			model.addAttribute("error", "Wrong channel");
 			return;
+		}
 		
-		
-		doGet(request, response);
-		
+		//System.out.println("Authentication: "+authentication);
+		//FWIW, THIS MUST BE DONE BECAUSE SECURITYCONTEXTHOLDER IS EMPTY WHEN METHOD IS REACHED VIA AJAX REQUEST !
+		if(SecurityContextHolder.getContext().getAuthentication() == null){
+			try {
+			    SecurityContext ctx = SecurityContextHolder.createEmptyContext();
+			    SecurityContextHolder.setContext(ctx);
+			    ctx.setAuthentication(authentication);
 	
+			}
+			finally{
+				System.out.println("Finished adding new securitycontextholder!");
+			}
+		}
+		//DEBUG
+		/*
+		for(Cookie cookie : request.getCookies())
+			System.out.println(cookie.getName() +":"+ cookie.getValue());
+		System.out.println("securitycontextholder->context: "+SecurityContextHolder.getContext());
+		System.out.println("securitycontextholder->context->authentication: "+SecurityContextHolder.getContext().getAuthentication());
+		System.out.println("contextHolderStrategy: "+SecurityContextHolder.getContextHolderStrategy());
+		System.out.println("getUserPrincipal: "+request.getUserPrincipal());
+		System.out.println("request.getAuthType(): "+request.getAuthType());
+		System.out.println("user-agent: "+request.getHeader("User-Agent"));
+		
+		System.out.println("requestURL: "+request.getRequestURL());
+		
+		System.out.println("first the channel: "+channelService.getChannel(name));
+		
+		*/
+		AtmosphereResource resource = (AtmosphereResource) request
+                .getAttribute(FrameworkConfig.ATMOSPHERE_RESOURCE);
+
+		this.doGet(resource, request, resource.getResponse());
+
+		
+		
         /*
         Channel returnChannel;
 		
@@ -243,25 +290,130 @@ public class JSONController {
 		if(wasFound == false)
 			return;
 		
-		
-		doPost(request, response);
+		this.doPost(request);
+		//doPost(request, response);
 		
 	}
 	
 	
-	 private void doGet(HttpServletRequest req, HttpServletResponse res)
+
+    // See AtmosphereHandlerPubSub example - same code as GET
+    private void doGet(
+                    AtmosphereResource r,
+                    HttpServletRequest req, HttpServletResponse res) {
+            // Log all events on the console, including WebSocket events.
+            r.addEventListener(new WebSocketEventListenerAdapter());
+
+            res.setContentType("text/html;charset=ISO-8859-1");
+
+            Broadcaster b = lookupBroadcaster(req.getPathInfo());
+            r.setBroadcaster(b);
+
+            String header = req.getHeader(HeaderConfig.X_ATMOSPHERE_TRANSPORT);
+            if (HeaderConfig.LONG_POLLING_TRANSPORT.equalsIgnoreCase(header)) {
+                    req.setAttribute(ApplicationConfig.RESUME_ON_BROADCAST,
+                                    Boolean.TRUE);
+                    r.suspend(-1);
+            } else {
+                    r.suspend(-1);
+            }
+    }
+
+    // See AtmosphereHandlerPubSub example - same code as POST
+    private void doPost(HttpServletRequest req) throws IOException {
+            Broadcaster b = lookupBroadcaster(req.getPathInfo());
+            String message = req.getReader().readLine();
+
+            if (message != null && message.indexOf("message") != -1) {
+                    b.broadcast(message.substring("message=".length()));
+            }
+    }
+
+    /**
+     * Retrieve the {@link Broadcaster} based on the request's path info.
+     * 
+     * @param pathInfo
+     * @return the {@link Broadcaster} based on the request's path info.
+     */
+    Broadcaster lookupBroadcaster(String pathInfo) {
+	    if (pathInfo == null) {
+	        return BroadcasterFactory.getDefault().lookup("/", true);
+	    } else {
+	        String[] decodedPath = pathInfo.split("/");
+	        return BroadcasterFactory.getDefault().lookup(
+	                decodedPath[decodedPath.length - 1], true);
+	    }
+    }
+
+    /*
+	 private void doGet(HttpServletRequest req, HttpServletResponse res, String channelName)
 		       throws IOException {
+		 
         // Create a Meteor
         Meteor m = Meteor.build(req);
 
         // Log all events on the console, including WebSocket events.
         m.addListener(new WebSocketEventListenerAdapter());
 
-        res.setContentType("text/html;charset=ISO-8859-1");
-
+        //res.setContentType("text/html;charset=UTF-8");
+        res.setContentType("text/plain");
         Broadcaster b = lookupBroadcaster(req.getRequestURI());
         m.setBroadcaster(b);
 
+        
+        
+        
+        //
+        Channel returnChannel;
+		
+        
+		//GET parameter UPDATE called
+		if(req.getParameter("update") != null){
+			
+			long age = Long.parseLong(req.getParameter("update"));
+			Channel c = new Channel();
+			c.setName(channelName);
+			c.setLastChanged(new Timestamp(age));
+			if(channelService.updateChannel(c)){
+				returnChannel = c;
+				
+				
+				System.out.println("did indeed update");	
+				
+			}
+			else { // no update necessary, send 304 NOT CHANGED back
+				res.setStatus(304);
+				System.out.println("did indeed cancel");
+				return;
+			}
+			
+		}
+		else {  // Nobody wanted to update, just get the channel
+			returnChannel = channelService.getChannel(channelName);
+		}
+		
+		PrintWriter out = res.getWriter();
+		
+		JsonGenerator gen = Json.createGenerator(out);
+		
+		
+	
+		gen.writeStartObject();
+		
+		gen.write("name", returnChannel.getName());
+		gen.write("text", returnChannel.getText());
+		gen.write("timestamp", returnChannel.getLastChanged().getTime());
+		
+		
+		
+		gen.writeEnd();
+		
+		gen.close();
+        
+        
+        //
+        
+        
         if (req.getHeader(HeaderConfig.X_ATMOSPHERE_TRANSPORT)
                 .equalsIgnoreCase(HeaderConfig.LONG_POLLING_TRANSPORT)) {
             req.setAttribute(ApplicationConfig.RESUME_ON_BROADCAST, Boolean.TRUE);
@@ -292,7 +444,7 @@ public class JSONController {
         return b;
     }
 	
-	
+	*/
 	
 	/**
 	 * handles requests for channel #otaniemi
